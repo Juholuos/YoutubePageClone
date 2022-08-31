4933e761bb01406ea6ca0cab1967164193c17106
Juholuos <juholuostarihw@gmail.com>
2022-08-31 14:04:43 +0300

    Update HauskaKyselyPeli.py

--- HauskaKyselyPeli.py
+++ HauskaKyselyPeli.py
@@ -1,10 +1,14 @@
 from time import sleep
 
 print("Kävelet illalla yksin tiellä..")
+print("\n")
 sleep(2)
 print("Yhtäkkiä kuulet juoksuaskelia takaasi, ja ennenkuin kerkeät edes kääntymään..")
+print("\n")
 sleep(3)
 print("Näkösi pimenee, ja seuraavana heräät pimeästä varastosta, kädet sidottuina")
+print("\n")
 sleep(3)
 print("Sinun tehtävänäsi on nyt päättää, mitä teet!")
+print("\n")
 
