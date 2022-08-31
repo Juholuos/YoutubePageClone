2df75dba0869d67904856789f7eca6d61c5ac0e1
Juholuos <juholuostarihw@gmail.com>
2022-08-31 13:26:43 +0300

    Revert "Lisäsin video-opetukset tähän kansioon"
    This reverts commit 521d257ddceecb4e6a4184fc6cdd5e17bd52e2c6.

--- Videonjutut/tiedostojenluku.py
+++
@@ -1,5 +0,0 @@
-työntekijätiedosto = open("työntekijät.txt", "r")
-
-print(työntekijätiedosto.readable())
-
-työntekijätiedosto.close()
--- Videonjutut/videolaskin (1).py
+++
@@ -1,8 +0,0 @@
-#plus lasku
-#int = kokonaisluvut
-#float = desimaalit
-num1 = float(input("Kirjoita numero: "))
-num2 = float(input("Kirjoita toinen numero: "))
-result = num1 + num2
-
-print (result)
