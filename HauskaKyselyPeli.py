ac51a05fcf040f5a51c6af018d705f693ad01757
Juholuos <juholuostarihw@gmail.com>
2022-08-31 14:02:05 +0300

    Aloitin hauskankyselypelin

---
+++ HauskaKyselyPeli.py
@@ -0,0 +1,10 @@
+from time import sleep
+
+print("Kävelet illalla yksin tiellä..")
+sleep(2)
+print("Yhtäkkiä kuulet juoksuaskelia takaasi, ja ennenkuin kerkeät edes kääntymään..")
+sleep(3)
+print("Näkösi pimenee, ja seuraavana heräät pimeästä varastosta, kädet sidottuina")
+sleep(3)
+print("Sinun tehtävänäsi on nyt päättää, mitä teet!")
+
