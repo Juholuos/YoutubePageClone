3197a351c2168724a70d8ae26d04c968c72ff86e
Juholuos <juholuostarihw@gmail.com>
2022-09-02 17:47:53 +0300

    Muutoksia

--- HauskaKyselyPeli.py
+++ HauskaKyselyPeli.py
@@ -14,7 +14,7 @@ sleep(1)
 
 def main():
         print("Huomaat, että maassa on puukko. \n")
-        kysymys1 = input("Yritätkö kerätä sen jaloilla (A), vai jatkatko huoneen tutkimista? (B): ")
+        kysymys1 = input("(A) Yritä kerätä se jaloilla / (B) Jatka huoneen tutkimista?: ")
         print("\n")
         if kysymys1 == ("A"):
                 print("Et saa nostettua puukkoa jaloilla, ja siitä tuleva kolina herättää toisessa huoneessa olevan nappaajasi huomion.\n")
@@ -28,10 +28,21 @@ def main():
                                 main()
                         else:
                                 print("Heippa")
+                                quit()
         elif kysymys1 ==("B"):
                         print("Tutkiessasi huonetta huomaat, että nappaaja ei tyhjentänyt taskujasi ja löydät linkkuveitsen, jolla saat leikattua kätesi vapaiksi.")  
                         sleep(1)
                         print("----------------------------------------------\n")
                         sleep(1)
+        else:
+                print("Väärä vastaus, yritä uudelleen")
+                sleep(1)
+                main()
 main()
 
+def toinen():
+        print("Vapauduttuasi liikut varovasti oven luokse kuuntelemaan onko sieppaaja lähellä.\n")
+        sleep(1)
+        print("Oven takaa kuuluu ääniä.")
+        sleep(1)
+        kysymys2 = input("(A) Odota hetki oven takana jos sieppaaja lähtisi muualle / (B) Ota viereinen metallitanko ja houkuttele sieppaaja luoksesi(B)")
