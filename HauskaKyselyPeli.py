1eeff65580e12240f2df0a56629254b30051a6c4
Juholuos <juholuostarihw@gmail.com>
2022-09-02 15:59:45 +0300

    RiviPäivityksiä

--- HauskaKyselyPeli.py
+++ HauskaKyselyPeli.py
@@ -13,24 +13,23 @@ print("----------------------------------------------\n")
 sleep(1)
 
 def main():
-    print("Huomaat, että maassa on puukko. \n")
-    kysymys1 = input("Yritätkö kerätä sen jaloilla (A), vai jatkatko huoneen tutkimista? (B): ")
+        print("Huomaat, että maassa on puukko. \n")
+        kysymys1 = input("Yritätkö kerätä sen jaloilla (A), vai jatkatko huoneen tutkimista? (B): ")
 
-    print("\n")
-    if kysymys1 == ("A"):
-            print("Et saa nostettua puukkoa jaloilla, ja siitä tuleva kolina herättää toisessa huoneessa olevan nappaajasi huomion.\n")
-    sleep(1)
-            print("Nappaaja tulee ja viiltää sinulta kurkun auki.")
-            kuollut = True
-    if kuollut == True:
-            print("Peli päättyi!")
-            uudelleen = input("Haluatko yrittää uudelleen? (K/E): ")
-            if uudelleen == "K":
-                    main()
-            else:
-                print("Heippa")
-    elif kysymys1 ==("B"):
-        print("Tutkiessasi huonetta huomaat, että nappaaja ei tyhjentänyt taskujasi ja löydät linkkuveitsen, jolla saat leikattua kätesi vapaiksi.")    
-
+        print("\n")
+        if kysymys1 == ("A"):
+                print("Et saa nostettua puukkoa jaloilla, ja siitä tuleva kolina herättää toisessa huoneessa olevan nappaajasi huomion.\n")
+        sleep(1)
+        print("Nappaaja tulee ja viiltää sinulta kurkun auki.")
+        kuollut = True
+        if kuollut == True:
+                print("Peli päättyi!")
+                uudelleen = input("Haluatko yrittää uudelleen? (K/E): ")
+                if uudelleen == "K":
+                        main()
+                else:
+                        print("Heippa")
+        elif kysymys1 ==("B"):
+                        print("Tutkiessasi huonetta huomaat, että nappaaja ei tyhjentänyt taskujasi ja löydät linkkuveitsen, jolla saat leikattua kätesi vapaiksi.")  
 
 main()
