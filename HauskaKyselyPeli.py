699026fcecdf488cd6387827d4b2db4e3d5a23d7
Juholuos <juholuostarihw@gmail.com>
2022-09-02 11:11:15 +0300

    muutoksia

--- HauskaKyselyPeli.py
+++ HauskaKyselyPeli.py
@@ -1,14 +1,33 @@
 from time import sleep
+kuollut = False
 
-print("Kävelet illalla yksin tiellä..")
-print("\n")
+print("Kävelet illalla yksin tiellä..\n")
 sleep(2)
-print("Yhtäkkiä kuulet juoksuaskelia takaasi, ja ennenkuin kerkeät edes kääntymään..")
-print("\n")
+print("Yhtäkkiä kuulet juoksuaskelia takaasi, ja ennenkuin kerkeät edes kääntymään..\n")
 sleep(3)
-print("Näkösi pimenee, ja seuraavana heräät pimeästä varastosta, kädet sidottuina")
-print("\n")
+print("Näkösi pimenee, ja seuraavana heräät pimeästä varastosta, kädet sidottuina\n")
 sleep(3)
-print("Sinun tehtävänäsi on nyt päättää, mitä teet!")
+print("Sinun tehtävänäsi on nyt päättää, mitä teet!\n")
+print("----------------------------------------------\n")
+sleep(1)
+
+
+print("Huomaat, että maassa on puukko. \n")
+    kysymys1 = input("Yritätkö kerätä sen jaloilla (A), vai jatkatko huoneen tutkimista? (B): ")
+
 print("\n")
+if kysymys1 == ("A"):
+        print("Et saa nostettua puukkoa jaloilla, ja siitä tuleva kolina herättää toisessa huoneessa olevan nappaajasi huomion.\n")
+sleep(1)
+print("Nappaaja tulee ja viiltää sinulta kurkun auki.")
+kuollut = True
+if kuollut == True:
+        print("Peli päättyi!")
+        uudelleen = input("Haluatko yrittää uudelleen? (K/E): ")
+        if uudelleen == "K":
+
+            
+                   
+elif kysymys1 ==("B"):
+        print("Tutkiessasi huonetta huomaat, että nappaaja ei tyhjentänyt taskujasi ja löydät linkkuveitsen, jolla saat leikattua kätesi vapaiksi.")
 
