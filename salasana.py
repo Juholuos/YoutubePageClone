9ab00f44347376df5a6d2864f20632d9beb59c12
Juholuos <juholuostarihw@gmail.com>
2022-09-19 13:25:14 +0300

    mäim

--- salasana.py
+++ salasana.py
@@ -1,9 +1,12 @@
+from time import sleep
 
 def main():
     salainensalasana = open("salainensalasana.txt")
     salasana = input("Syötä salasana: ")
     if salasana == salainensalasana.read():
         print("Pääsy hyväksytty!")
+        sleep(2)
+        
     else:
         print("Pääsy hylätty!")
         main()
