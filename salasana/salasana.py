9cd35d2c626626c6a800c111d4072c47c97f9ce9
Juholuos <juholuostarihw@gmail.com>
2022-09-19 15:52:56 +0300

    toimii

--- salasana/salasana.py
+++ salasana/salasana.py
@@ -1,9 +1,17 @@
+from time import sleep
 def main():
-    salainensalasana = open("salainensalasana.txt")
-    salasana = input("Syötä salasana: ")
-    if salasana == salainensalasana.read():
-         print("Pääsy hyväksytty!")
-    else:
-        print("Pääsy hylätty!")
-        main()
+    #seuraavaksi teen yrityslaskurin
+        salainensalasana = open("salainensalasana.txt")
+        salasana = input("Syötä salasana: ")
+        if salasana == salainensalasana.read():
+            print("Pääsy hyväksytty!")
+            sleep(1)
+            print("Tiedoston sisältö on seuraavanlainen:")
+            sleep(1)
+            f = open("salainentiedosto.txt", "r")
+            sisältö = f.read()
+            print(sisältö)
+        else:
+            print("Pääsy hylätty!")
+            main()
 main()
